83654d2cc54d2bdc2316b8c7f560aa0ccc546e93
nikez <nikegreen@yandex.ru>
2023-01-17 12:11:10 +0500

    0. ToOne [#6873] del ' DEFAULT 1' from db\script\004_ddl_add_column_user_to_table_items.sql

--- db/scripts/004_ddl_add_column_user_to_table_items.sql
+++ db/scripts/004_ddl_add_column_user_to_table_items.sql
@@ -1,2 +1,3 @@
-alter table tasks add column user_id int not null references todo_user (id) DEFAULT 1;
+alter table tasks add column user_id int not null references todo_user (id);
 comment on column tasks.user_id is 'id пользователя создавшего задачу из таблицы todo_user';
+
